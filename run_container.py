7318c39640bf93611e4039be62e255214693af0f
Daniel Nakhimovich <danielnak@tutanota.com>
2023-08-08 16:11:59 -0400

    models outside of docker container

--- run_container.py
+++ run_container.py
@@ -23,6 +23,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "-mount", type=str, help="mount source launch file or directory"
     )
+    parser.add_argument(
+        "-models", type=str, help="mount directory for Detic pretrained models"
+    )
     parser.add_argument("-name", type=str, help="launch file name")
     parser.add_argument(
         "-host", type=str, default="pr1040", help="host name or ip-address"
@@ -37,6 +40,10 @@ if __name__ == "__main__":
     mount_path_str: Optional[str] = args.mount
     assert mount_path_str is not None
     mount_path = Path(mount_path_str)
+
+    models_path: Optional[str] = args.models
+    if models_path is not None:
+        models_path = Path(models_path)
 
     launch_file_name: Optional[str] = args.name
     assert launch_file_name is not None
@@ -53,9 +60,15 @@ if __name__ == "__main__":
         else:
             shutil.copyfile(mount_path, tmp_launch_path)
 
+        if models_path is not None:
+            mount_models_str="-v {}:{}/models".format(models_path,_DETIC_ROS_ROOT_INSIDE_CONTAINER)
+        else:
+            mount_models_str=""
+
         docker_run_command = """
             docker run \
                 -v {tmp_launch_path}:{detic_ros_root}/launch \
+                {maybe_mount_models_path} \
                 --rm --net=host -it \
                 --gpus 1 detic_ros:latest \
                 /bin/bash -i -c \
@@ -66,6 +79,7 @@ if __name__ == "__main__":
                 """.format(
             tmp_launch_path=tmp_launch_path,
             detic_ros_root=_DETIC_ROS_ROOT_INSIDE_CONTAINER,
+            maybe_mount_models_path=mount_models_str,
             host=args.host,
             launch_file_name=launch_file_name,
             launch_args=launch_args,
